7465e0138b088ee0e28800620aa244ccff2d9589
Wolf Mermelstein <wolf@404wolf.com>
2025-02-27 00:37:04 -0500

    update group id

--- darwin.nix
+++ darwin.nix
@@ -5,54 +5,9 @@
 }: let
   user = "%USER%";
 in {
-  imports = [
-    ../../modules/darwin/home-manager.nix
-    ../../modules/shared
-  ];
-
-  nix = {
-    package = pkgs.nix;
-
-    settings = {
-      trusted-users = ["@admin" "${user}"];
-      substituters = ["https://nix-community.cachix.org" "https://cache.nixos.org"];
-      trusted-public-keys = ["cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="];
-    };
-
-    gc = {
-      automatic = true;
-      interval = {
-        Weekday = 0;
-        Hour = 2;
-        Minute = 0;
-      };
-      options = "--delete-older-than 30d";
-    };
-
-    extraOptions = ''
-      experimental-features = nix-command flakes
-    '';
-  };
-
   system.checks.verifyNixPath = false;
 
-  environment.systemPackages = with pkgs;
-    [
-      emacs-unstable
-    ]
-    ++ (import ../../modules/shared/packages.nix {inherit pkgs;});
-
-  launchd.user.agents.emacs.path = [config.environment.systemPath];
-  launchd.user.agents.emacs.serviceConfig = {
-    KeepAlive = true;
-    ProgramArguments = [
-      "/bin/sh"
-      "-c"
-      "/bin/wait4path ${pkgs.emacs}/bin/emacs && exec ${pkgs.emacs}/bin/emacs --fg-daemon"
-    ];
-    StandardErrorPath = "/tmp/emacs.err.log";
-    StandardOutPath = "/tmp/emacs.out.log";
-  };
+  ids.gids.nixbld = 350;
 
   system = {
     stateVersion = 4;
